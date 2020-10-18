f4e448c24aaef9053cb9c4a27f936dd995504a0c
antoan <antoan.bekele@gmail.com>
2020-10-18 16:44:50 +0100

    Added new roam config

--- config.el
+++ config.el
@@ -232,11 +232,23 @@
 ;; (setq org-roam-graph-exclude-matcher '("private" "dailies"))
 
   (use-package! org-roam
-    :config
-    (setq org-roam-graph-viewer "/usr/bin/chromium-browser")
-    ;;Other options included 'ido, and 'ivy'.
-    (setq org-roam-completion-system 'helm))
-
+      :ensure t
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "/mnt/Blue/org/roam")
+      :config
+      (setq org-roam-graph-viewer "/usr/bin/chromium-browser")
+      ;;Other options included 'ido, and 'ivy'.
+      (setq org-roam-completion-system 'helm)
+      (add-to-list 'exec-path "/usr/bin/sqlite3")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
 ;; Encryption (via GPG) can be enabled for all new files by setting org-roam-encrypt-files to t.
 
 (use-package! org-protocol)
@@ -404,6 +416,7 @@
 
 (menu-bar-mode 1)
 
+;; TODO put in separate usr lisp file. set this up.
 (defun ros-catkin-make (dir)
   "Run catkin_make command in DIR."
   (interactive (list (read-directory-name "Directory: ")))
